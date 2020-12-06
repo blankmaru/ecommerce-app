d70f3d79f414a2338005d47137490e0c1220ea71
blankmaru <coopskywavy@gmail.com>
2020-12-06 23:05:58 +0200

    Add route

--- src/server.ts
+++ src/server.ts
@@ -68,10 +68,8 @@ passport.deserializeUser(async (id: string, done) => {
 
 app.use('/api/users', userRoutes)
 
-app.use(express.static("../../client/build"));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(`../${__dirname}`, 'build', 'index.html'));
+app.get('/', (req, res) => {
+    res.send('<h3>Page</h3>')
 });
 
 const port = process.env.PORT || 5000
