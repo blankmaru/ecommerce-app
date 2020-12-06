f705c31af1f597d7ea23542fa17eb833e2604d94
blankmaru <coopskywavy@gmail.com>
2020-12-06 23:27:21 +0200

    Upd. 3 for nginx server

--- src/server.ts
+++ src/server.ts
@@ -68,12 +68,6 @@ passport.deserializeUser(async (id: string, done) => {
 
 app.use('/api/users', userRoutes)
 
-app.use(express.static("../../client/build"));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(`../${__dirname}`, 'build', 'index.html'));
-});
-
 const port = process.env.PORT || 5000
 
 app.listen(port)
