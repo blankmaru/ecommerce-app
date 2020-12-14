aadc62695115a3446900f4ae738dad73381fc5fc
blankmaru <coopskywavy@gmail.com>
2020-12-14 23:35:56 +0200

    Adding multer storage and image uploading

--- src/server.ts
+++ src/server.ts
@@ -77,6 +77,87 @@ passport.deserializeUser(async (id: string, done) => {
     });
 })
 
+// Uploading files
+
+const conn = mongoose.createConnection(mongoURI);
+
+import multer from 'multer';
+import Grid from 'gridfs-stream';
+import GridFsStorage from 'multer-gridfs-storage';
+
+let gfs: any;
+
+conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+})
+
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req: any, file: any) => {
+      return {
+        filename: 'file_' + Date.now()
+      };
+    }
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send({ file: req.file })
+})
+
+app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err: Error, files: any) => {
+        if (!files || files.length === 0) {
+            return res.status(404).json({
+              err: 'No files exist'
+            });
+        }
+      
+        return res.json(files);
+    })
+})
+
+app.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err: Error, file: any) => {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+              err: 'No file exist'
+            });
+        }
+
+        return res.json(file);
+    })
+})
+
+app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err: Error, file: any) => {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+              err: 'No file exists'
+            });
+        }
+
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);               
+        } else {
+            res.status(404).json({
+                err: 'Not an image'
+            });
+        }
+    })
+})
+
+app.delete('/files/:id', (req, res) => {
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err: Error, gridStore: any) => {
+        if (err) {
+            return res.status(404).json({ err: err })
+        }
+    })
+})
+
 app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/category', categoryRoutes)
