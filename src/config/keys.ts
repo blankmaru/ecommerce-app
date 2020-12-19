0905a25acc124a03b526b0c5acee720e7776abef
blankmaru <coopskywavy@gmail.com>
2020-12-19 17:46:07 +0200

    keys change

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,6 +1,6 @@
 export const mongoURI = 'mongodb+srv://blankmaru:blank123@template-cluster.nkiqa.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
-export const GMAIL_USER = 'getnochan@gmail.com'
-export const GMAIL_USER_PASSWORD = 'zek228322'
+export const GMAIL_USER = ''
+export const GMAIL_USER_PASSWORD = ''
 export const TWILIO_ACCOUNT_SID = 'AC5da716b843c7fb23ba4173422f6c65b8'
 export const TWILIO_AUTH_TOKEN = 'cfdd71e1d8343185a4174222f9f824a8'
 export const TWILIO_PHONE_NUMBER = '+18289444812'
